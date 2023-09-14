9af83256db1c18ee63b67ec53cc710f6df0cd4a4
Joshua Mather <jmather0707@gmail.com>
2023-09-14 11:12:36 +0100

    Add comps

---
+++ comps/src/App.js
@@ -0,0 +1,39 @@
+import Button from "./components/Button";
+import { GoBell, GoCloud, GoDatabase } from 'react-icons/go';
+
+function App() {
+    return (
+        <div>
+            <div>
+                <Button primary rounded outline className="mb-5">
+                    <GoBell />
+                    Button 1
+                </Button>
+            </div>
+            <div>
+                <Button danger outline>
+                    <GoCloud />
+                    Button 2
+                </Button>
+            </div>
+            <div>
+                <Button warning>
+                    <GoDatabase />
+                    Button 3
+                </Button>
+            </div>
+            <div>
+                <Button success outline>
+                    Button 4
+                </Button>
+            </div>
+            <div>
+                <Button secondary rounded>
+                    Button 5
+                </Button>
+            </div>
+        </div>
+    )
+}
+
+export default App;
