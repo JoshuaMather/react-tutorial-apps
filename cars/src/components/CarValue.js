6fd5a521e5330aebc5ff2dfa8f7c4268b66e836d
Joshua Mather <jmather0707@gmail.com>
2023-09-20 16:13:43 +0100

    Section 20 - Redux toolkit multiple slices

--- cars/src/components/CarValue.js
+++ cars/src/components/CarValue.js
@@ -1,7 +1,27 @@
+import { useSelector } from 'react-redux'
+
 function CarValue() {
+    // const totalCost = useSelector(({ cars: { data, searchTerm } }) => {
+    //     data.filter((car) => {
+    //         car.name.toLowerCase().includes(searchTerm.toLowerCase())
+    //     }).reduce((acc, car) => acc + car.cost, 0)
+    // })
+
+    const totalCost = useSelector(({ cars: { data, searchTerm } }) => {
+        const filteredCars = data.filter((car) => {
+            return car.name.toLowerCase().includes(searchTerm.toLowerCase());
+        });
+
+        return filteredCars.reduce((acc, car) => {
+            return acc + car.cost
+        }, 0);
+    })
+
+    console.log(totalCost);
+
     return (
-        <div>
-            CarValue
+        <div className="car-value">
+            Total Cost: ${totalCost}
         </div>
     );
 }
